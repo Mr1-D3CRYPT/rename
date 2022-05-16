3b6bdc8394d9c4c04d6e38c41002a8389ee7129a
Mr1 <64800911+Mr1-D3CRYPT@users.noreply.github.com>
2022-05-16 23:36:57 +0530

    Update rn.py

--- rn.py
+++ rn.py
@@ -1,13 +1,17 @@
 import os
 
+#get the path of the file and the file names
 path = os.getcwd()
-
-print(path)
 files = os.listdir(path)
 
-print(f"Before Renaming: {files}")
+#print the file names before iteration
+print(f"Before Renaming: {files}") 
+
+#rename the files
 for i in range(len(files)):
 	if files[i].find(".py") != -1 :
 		continue
-	os.rename(files[i],f"night{i+1}.jpg")
+	os.rename(files[i],f"parentfilename{i+1}.extension")
+	
+#print the final file names
 print(f"After Renaming: {os.listdir(path)}")
